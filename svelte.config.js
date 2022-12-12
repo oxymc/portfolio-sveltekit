cf6f9f7e0b5a602c619fa20271f98f3828c07ba3
Max <skiff4ek@gmail.com>
2022-12-12 12:30:42 +0200

    removed gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,10 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
-
-const dev = process.argv.includes('dev');
-
+import adapter from '@sveltejs/adapter-auto';
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
-	kit: {
-		 adapter: adapter({
-		   pages: 'build',
-		   assets: 'build',
-		   fallback: null
-		 }),
-		 paths: {
-			base: dev ? '' : '/portfolio-sveltekit',
-		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal',
-		}
+  kit: {
+    adapter: adapter()
+  }
 };
-
+ 
 export default config;
